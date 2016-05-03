722aae7efff205cd9f30b24ee7c68143fad76dbf
Max Clark <maxaclark@gmail.com>
2016-05-03 14:45:36 -0400

    Made Greyhound ship with keyboard movement

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float moveForce = 20f;
+
+	private Vector2 direction = Vector2.zero;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+
+	void FixedUpdate () {
+		rb.AddForce (direction * moveForce);
+	}
+}
