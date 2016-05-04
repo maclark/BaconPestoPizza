9a6d08f8f03fe5f2c7cfbc920793297cc8ecb673
Max Clark <maxaclark@gmail.com>
2016-05-03 22:53:01 -0400

    Got back to twin stick functionality. Imported an objectpooling script, but haven't implemented it.

---
+++ Assets/Bullets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Vector2 direction = Vector2.zero;
+	public float moveForce = 100f;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		FireBullet ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetDirection (Vector2 dir) {
+		direction = dir;
+		direction.Normalize ();
+	}
+
+	public void FireBullet() {
+		rb.AddForce ( direction * moveForce);
+	}
+}
--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public float moveForce = 20f;
+	public Bullet bulletPrefab;
 
 	private Vector2 direction = Vector2.zero;
 	private Rigidbody2D rb;
@@ -16,6 +17,13 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		direction = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Vector2 aim = new Vector2( Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"));
+		print (aim);
+
+		if (Input.GetAxis ("RightTrigger") > 0) {
+			Bullet b = (Bullet)Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+			b.SetDirection (aim);
+		}
 	}
 
 	void FixedUpdate () {
