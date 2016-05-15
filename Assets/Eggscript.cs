e4735f6fd9fcc78393e7abf79e70f82fa4c6e5f9
Matthew Seaton <matthew.j.seaton@gmail.com>
2016-05-15 14:37:15 -0400

    Initial Egg with repeated function call added

---
+++ Assets/Eggscript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eggscript : MonoBehaviour {
+	public Time age;
+	public float birthTime;
+	public float EggHatchDelay=30.0f;
+	public float RollInterval=15.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void Awake (){
+		birthTime = Time.realtimeSinceStartup;
+		InvokeRepeating ("Egg_Roll", EggHatchDelay, RollInterval);
+
+	}
+	void Egg_Roll(){
+		//Instantiate new ship (bird) object,then kill the egg
+		if (GetComponent<SpriteRenderer> ().color == Color.red) {
+
+			GetComponent<SpriteRenderer> ().color = Color.gray;
+		} else if (GetComponent<SpriteRenderer> ().color != Color.green) {
+			GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+		int roll = Random.Range (0, 100);
+		if (roll >= 75) {
+			GetComponent<SpriteRenderer> ().color = Color.green;
+		}
+		RollInterval /= 2;
+	}
+}
