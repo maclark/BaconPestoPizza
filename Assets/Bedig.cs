ea633bd80e9c1480900546f18c61daf7bc805cad
Max Clark <max.a.clark@gmail.com>
2016-07-01 13:38:42 -0400

    Created empty scenes. Created the Bedig enemy.

---
+++ Assets/Bedig.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bedig : Flyer {
+	public float skew = 2f;
+
+	void Awake() {
+		base.OnAwake ();
+	}
+
+	void Start () {
+		base.OnStart ();
+	}
+
+	void Update () {
+		base.OnUpdate ();
+	}
+
+	void FixedUpdate () {
+		Hunt ();
+		//base.OnFixedUpdate ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		base.TriggerEnter2D (other);
+	}
+
+	void OnBecameVisible () {
+		base.BecameVisible ();
+	}
+
+	void OnBecameInvisible () {
+		base.BecameInvisible ();
+	}
+
+	public override void FireBullet() {
+		if (target == null) {
+			SetNearestTarget ();
+			if (target == null) {
+				CancelInvoke ();
+				stopFiring = true;
+				return;
+			}
+		}
+		Bullet bullet = gm.GetComponent<ObjectPooler> ().GetPooledObject ().GetComponent<Bullet> ();
+		bullet.gameObject.SetActive (true);
+		Vector2 skewVector = new Vector2 (Random.Range (-skew, skew), Random.Range (-skew, skew));
+		Vector2 aim = target.position - transform.position;
+		bullet.Fire (transform.position, aim + skewVector);	
+	}
+
+	public override void Die () {
+		base.Die ();
+	}
+
+	void Hunt () {
+		if (target) {
+			
+		}
+
+	}
+}
---
+++ Assets/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level auto load disabled, use a postive number is seconds");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
